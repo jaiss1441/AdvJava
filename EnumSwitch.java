7d9b1fbaba285fe020ae4358ab00f97120cc4431
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-10-03 23:54:04 +0530

    Enum with Switch Statements

---
+++ EnumSwitch.java
@@ -0,0 +1,30 @@
+enum Status2{
+    STARTED,
+    IN_PROGRESS,
+    COMPLETED,
+    FAILED
+}
+
+public class EnumSwitch {
+    public static void main(String[] args) {
+        Status2 currentStatus2 = Status2.STARTED;
+
+        switch (currentStatus2){
+          case STARTED:
+                System.out.println("The process has Started.");
+                break;
+            case IN_PROGRESS:
+                System.out.println("The Process is In Progress.");
+                break;
+            case COMPLETED:
+                System.out.println("The Process is Completed.");
+                break;  
+            case FAILED:
+                System.out.println("The Process has Failed.");
+                break;      
+            default:
+                System.out.println("Unknown Status.");
+                break;
+        }
+    }
+}
