1559d9e778e43049ed3364eeace38101b6c05469
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-10-05 00:29:26 +0530

    Enum Class Example

---
+++ EnumClassExp.java
@@ -0,0 +1,11 @@
+enum Laptop5{
+    DELL,HP,APPLE,ASUS
+}
+
+public class EnumClassExp {
+    public static void main(String[] args) {
+            Laptop5 myLaptop = Laptop5.DELL;
+            System.out.println("My Laptop is: " + myLaptop);
+    }
+    
+}
