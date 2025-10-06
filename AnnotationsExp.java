a8aaa589632c4f67300b5f28ebe79cb7bdf82f62
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-10-06 23:55:18 +0530

    Why we use Annotation

--- AnnotationsExp.java
+++ AnnotationsExp.java
@@ -13,6 +13,8 @@ class A4{
 //         System.out.println("In Class B");    // it will show error which can help you to catch bug/error
 //     }
 // }
+
+// Annottion nothing but meta information to compiler to understand what we wanna do 
 class B4 extends A4{
     @Override
     public void ThisIsExampleOfAnnotation(){
