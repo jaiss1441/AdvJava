3ed3010ca85feaffda938f14d02845be7bbae68b
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-10-06 23:53:45 +0530

    Why we use Annotation

---
+++ AnnotationsExp.java
@@ -0,0 +1,28 @@
+class A4{
+
+    public void ThisIsExampleOfAnnotation(){
+        System.out.println("In Class A");
+    }
+}
+    //here it shoud show class B4 method but it showing Class A4 method becouse of method wad diffrent
+    //If we add (@override) annotation here it will show error and then we can fix it 
+    
+// class B4 extends A4{
+//     @Override
+//     public void ThisIsExampleOfAnnotations(){ // here extra "s" is added into method if we add @Ocerride annotation 
+//         System.out.println("In Class B");    // it will show error which can help you to catch bug/error
+//     }
+// }
+class B4 extends A4{
+    @Override
+    public void ThisIsExampleOfAnnotation(){
+        System.out.println("In Class B");
+    }
+}
+
+public class AnnotationsExp{
+    public static void main (String[] args){
+        B4 obj = new B4();
+        obj.ThisIsExampleOfAnnotation();
+    }
+}
