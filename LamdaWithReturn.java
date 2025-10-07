24537c981e24fcdf9ffc14ee00af2bec81dc8cb0
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-10-08 01:09:14 +0530

    Lamda with return

--- LamdaWithReturn.java
+++ LamdaWithReturn.java
@@ -5,13 +5,18 @@ interface Lam2{
 
 public class LamdaWithReturn {
     public static void main(String[] args) {
-        Lam2 obj = new Lam2()
-         {
-            public int add( int i, int j){
-                System.out.println("In Lamda ");
-                return i+j;
-            }
-        };              //Annonimous class with return type
+        // Lam2 obj = new Lam2()
+        //  {
+        //     public int add( int i, int j){
+        //         System.out.println("In Lamda ");
+        //         return i+j;
+        //     }
+        // };              //Annonimous class with return type
+
+        // int result = obj.add(5, 4);
+        // System.out.println(result);
+
+        Lam2 obj = (int i, int j) -> i + j; // with lamda reduced version 
 
         int result = obj.add(5, 4);
         System.out.println(result);
