817495c0875f37be5263747e6ecd232f0935a25c
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-10-08 01:03:55 +0530

    Annonimous class with return type

---
+++ LamdaWithReturn.java
@@ -0,0 +1,19 @@
+@FunctionalInterface
+interface Lam2{
+    int add(int i, int j);
+}
+
+public class LamdaWithReturn {
+    public static void main(String[] args) {
+        Lam2 obj = new Lam2()
+         {
+            public int add( int i, int j){
+                System.out.println("In Lamda ");
+                return i+j;
+            }
+        };              //Annonimous class with return type
+
+        int result = obj.add(5, 4);
+        System.out.println(result);
+    }
+}
