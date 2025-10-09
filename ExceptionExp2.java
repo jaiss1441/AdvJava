fa11ec678955b58cf0ba9c4a5eb226a3e253f29d
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-10-10 00:21:51 +0530

    Try with multiple Catch Block

--- ExceptionExp2.java
+++ ExceptionExp2.java
@@ -2,11 +2,13 @@ public class ExceptionExp2 {
     public static void main(String[] args) {
         int i = 2;
         int j = 0;
+        String str = null;
 
         int arr[] = new int[5];
 
         try {
             j = 18 / i;
+            System.out.println(str.length());
             System.out.println("Array Out Of Bond" + arr[5]);
 
         } catch (ArithmeticException e) {
@@ -14,6 +16,9 @@ public class ExceptionExp2 {
         }
         catch(ArrayIndexOutOfBoundsException e){
             System.out.println(" Array Index Out Of Bond Exception");
+        }
+        catch(NullPointerException e){
+            System.out.println("Null Pointer Exception");
         }
     }
 }
