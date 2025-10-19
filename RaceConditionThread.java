ee02b3b896a901315e05f6bcdf9db1ae32c3aef5
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-10-19 23:55:38 +0530

    Runing Condition In MultiThreading

---
+++ RaceConditionThread.java
@@ -0,0 +1,31 @@
+class Counter{
+    int count = 0;
+    public void increment(){
+        count++;
+    }
+}
+public class RaceConditionThread {
+    public static void main(String[] args) {
+        Counter c = new Counter();
+
+        Runnable in = () ->{
+            for(int i = 0; i < 1000; i++){
+                c.increment();
+            }
+        };
+
+        Runnable out = () ->{
+            for(int i = 0; i < 1000; i++){
+                c.increment();
+            }
+        };
+
+        Thread t1 = new Thread(in);
+        Thread t2 = new Thread(out);
+        t1.start();
+        t2.start();
+
+        System.out.println(c.count);
+
+    }
+}
