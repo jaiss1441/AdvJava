ae54504d62e375ed1f27daac9b1ca0d9b8ace311
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-10-29 00:52:36 +0530

    Sorting Collection Objrct/Values with help Comparator INterface Implementing Anonumus Class with Lamda Expression

---
+++ ComparatorWithAnonymusClassLamda.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorWithAnonymusClassLamda {
+    //It is use to Short Collection wiht help Comparetor Interface it is Functional InterFace
+    public static void main(String[] args) {
+
+        Comparator<Integer> com = (i,j) ->
+            {
+                if(i%10 > j%10)
+                return 1;
+                else
+                return -1;
+            };
+        List<Integer> num = new ArrayList<>();
+
+        num.add(30);
+        num.add(81);
+        num.add(75);
+        num.add(14);
+
+        //Collections.sort(num);
+        //sort() method is used to short Collection Object/Values
+        //by default sort() method have its own sorting logic 
+        //if we want to write our own login we use Comparator Interface
+
+        Collections.sort(num,com);
+        System.out.print(num);
+    }
+}
