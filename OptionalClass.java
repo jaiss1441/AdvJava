7a5ce05092e0b1ae9fbdf512666508fb60ca80c8
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2025-11-06 02:41:59 +0530

    Optional class introduce in  1.8

--- OptionalClass.java
+++ OptionalClass.java
@@ -4,16 +4,23 @@ import java.util.Optional;
 
 public class OptionalClass {
     public static void main(String[] args) {
-        List<String> names = List.of("Alice", "Bob", "harlie", "David", "Eve");    
-        Optional<String> firstNameStartingWithC = names.stream()
-                                                    .filter(name -> name.startsWith("C"))
-                                                    .findFirst();
-        System.out.println(firstNameStartingWithC.orElse("Not Found"));
+        List<String> names = List.of("Alice", "Bob", "harlie", "David", "Eve"); 
         
-       String con = names.stream()
+        
+         String con = names.stream()
                             .filter(name -> name.contains("i"))
                             .findFirst()
                             .orElse("Not found");
         System.out.println(con);
+
+
+        Optional<String> firstNameStartingWithC = names.stream()
+                                                    .filter(name -> name.startsWith("C"))
+                                                    .findFirst();
+        System.out.println(firstNameStartingWithC);
+        
+      
 }
+//optional classes introduce in 1.8
+//in optional, string can be null it did't give you nullpointer exception
 }
